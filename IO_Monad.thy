74ad068ee8fda705012da7d09f21c59285e470a5
diekmann <diekmann@net.in.tum.de>
2017-08-12 15:20:33 +0200

    [DRAFT] also somehow works for SML

---
+++ IO_Monad.thy
@@ -0,0 +1,76 @@
+(*DRAFT!*)
+theory IO_Monad
+  imports Main
+    "~~/src/HOL/Library/Monad_Syntax"
+    "~~/src/HOL/Library/Code_Char"
+begin
+
+(*Programming TLS in Isabelle/HOL by Andreas Lochbihler and Marc Züst uses a partial function (\<rightharpoonup>)!
+typedecl real_world
+typedef 'a IO = "UNIV :: (real_world \<rightharpoonup> 'a \<times> real_world) set" by simp
+*)
+
+
+(*https://wiki.haskell.org/IO_inside*)
+
+typedecl real_world -- \<open>fake type. Dangerous.\<close>
+
+text \<open>The set of all functions which take a @{typ real_world} and return an @{typ 'a} and a @{typ real_world}.\<close>
+typedef 'a IO = "UNIV :: (real_world \<Rightarrow> 'a \<times> real_world) set"
+proof -
+  show "\<exists>x. x \<in> UNIV" by simp
+qed
+
+term Abs_IO
+term Rep_IO
+
+  lemma "do { x \<leftarrow> Some (1::nat); Some x} = Some 1" by simp
+
+text\<open>Make sure the code generator does not try to define @{typ "'a IO"} by itself, but always uses
+     The full qualified Prelude.IO\<close>
+code_printing type_constructor IO \<rightharpoonup> (Haskell) "Prelude.IO _"
+                                 and (SML) "_"
+code_reserved Haskell IO
+code_reserved SML IO
+
+text\<open>Define a constant in Isabelle and provide a Haskell module which implements it.
+
+In Isabelle, a @{typ string} is just a type synonym for @{typ "char list"}.
+Consequently, translating a @{typ string} to Haskell yields a [Prelude.Char].
+The Isabelle @{typ String.literal} gets translated to a Haskell String.
+\<close>
+consts println :: "String.literal \<Rightarrow> unit IO"
+code_printing constant println \<rightharpoonup> (Haskell) "StdIO.println"
+                              and (SML) "print (_ ^ \"\\n\")" (*adding newline manually*)
+code_printing code_module StdIO \<rightharpoonup> (Haskell) {*
+import qualified Prelude (putStrLn);
+println = Prelude.putStrLn;
+*}
+code_reserved Haskell println StdIO
+code_reserved SML println print
+
+text\<open>The main function, defined in Isabelle. It should have the right type in Haskell.\<close>
+definition main :: "unit IO" where
+  "main \<equiv> println (String.implode ''Hello World!'')"
+  
+  (*
+setup_lifting type_definition_IO
+value "term_of_class.term_of (x::real_world)"
+value "term_of_class.term_of x"
+value "term_of_class.term_of (Rep_IO::unit IO \<Rightarrow> real_world \<Rightarrow> unit \<times> real_world)"
+value[code] "main"
+  *)
+export_code main in Haskell
+export_code main in Haskell module_name "Main" file "code"
+(*
+  $ cd code
+  $ runhaskell code/Main.hs
+*)
+  
+  
+export_code main in SML
+export_code main in SML file test.sml
+(*
+  $ LD_PRELOAD=~/bin/Isabelle2016-1/contrib/polyml-5.6-1/x86-linux/libgmp.so.3  ~/bin/Isabelle2016-1/contrib/polyml-5.6/x86-linux/poly --use test.sml
+*)
+end

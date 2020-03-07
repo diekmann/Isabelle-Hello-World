a3374fcb52b2d1c25105b5b6e9f05dd3ec153061
diekmann <diekmann>
2020-03-07 21:03:42 +0100

    Make build hermetic.
    Don't write to hard-coded /tmp, use $ISABELLE_TMP. Well, this makes the
    build as hermetic as it gets when writing to the filesystem.

--- HelloWorld.thy
+++ HelloWorld.thy
@@ -26,23 +26,31 @@ text\<open>Checking that the generated code compiles.\<close>
 export_code main checking Haskell? SML
 
 
+ML_val\<open>Isabelle_System.bash "echo ${ISABELLE_TMP} > ${ISABELLE_TMP}/self"\<close>
+text\<open>
+During the build of this session, the code generated in the following subsections will be
+written to
+\<close>
+text_raw\<open>\verbatiminput{${ISABELLE_TMP}/self}\<close>
+
+
 subsection\<open>Haskell\<close>
 
-export_code main in Haskell file "/tmp/exported_hs"
+export_code main in Haskell file "$ISABELLE_TMP/exported_hs"
 
-text\<open>The generated helper module \<^file>\<open>/tmp/exported_hs/StdIO.hs\<close> is shown below.\<close>
-text_raw\<open>\verbatiminput{/tmp/exported_hs/StdIO.hs}\<close>
+text\<open>The generated helper module \<^file>\<open>$ISABELLE_TMP/exported_hs/StdIO.hs\<close> is shown below.\<close>
+text_raw\<open>\verbatiminput{$ISABELLE_TMP/exported_hs/StdIO.hs}\<close>
  
-text\<open>The generated main file \<^file>\<open>/tmp/exported_hs/HelloWorld.hs\<close> is shown below.\<close>
-text_raw\<open>\verbatiminput{/tmp/exported_hs/HelloWorld.hs}\<close>
+text\<open>The generated main file \<^file>\<open>$ISABELLE_TMP/exported_hs/HelloWorld.hs\<close> is shown below.\<close>
+text_raw\<open>\verbatiminput{$ISABELLE_TMP/exported_hs/HelloWorld.hs}\<close>
 
 
 subsection\<open>SML\<close>
 
-export_code main in SML file "/tmp/exported.sml"
+export_code main in SML file "$ISABELLE_TMP/exported.sml"
 
-text\<open>The generated SML code in \<^file>\<open>/tmp/exported.sml\<close> is shown below.\<close>
-text_raw\<open>\verbatiminput{/tmp/exported.sml}\<close>
+text\<open>The generated SML code in \<^file>\<open>$ISABELLE_TMP/exported.sml\<close> is shown below.\<close>
+text_raw\<open>\verbatiminput{$ISABELLE_TMP/exported.sml}\<close>
 
 
 end
